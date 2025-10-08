53b0b19cbe6409af2371b6f0721e7c6f7dd7f69c
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-10-08 22:40:24 +0200

    prepare 1.21.10 port

--- 1.21.9/Common/src/main/java/fuzs/universalbonemeal/client/UniversalBoneMealClient.java
+++
@@ -1,7 +0,0 @@
-package fuzs.universalbonemeal.client;
-
-import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
-
-public class UniversalBoneMealClient implements ClientModConstructor {
-
-}
