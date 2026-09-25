38db5bb102617c6534af64a68109278316fc94cb
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-09-25 19:16:01 +0200

    extend StateTestingPredicate

--- Common/src/main/java/fuzs/universalbonemeal/common/world/level/levelgen/blockpredicates/BlockPropertyPredicate.java
+++ Common/src/main/java/fuzs/universalbonemeal/common/world/level/levelgen/blockpredicates/BlockPropertyPredicate.java
@@ -5,28 +5,32 @@ import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import fuzs.universalbonemeal.common.init.ModRegistry;
 import fuzs.universalbonemeal.common.world.level.block.bonemeal.LazyBlockProperty;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.core.Vec3i;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
-import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicate;
 import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicateType;
+import net.minecraft.world.level.levelgen.blockpredicates.StateTestingPredicate;
 import org.jspecify.annotations.Nullable;
 
-public final class BlockPropertyPredicate implements BlockPredicate, LazyBlockProperty {
-    public static final MapCodec<BlockPropertyPredicate> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
-                    Codec.STRING.fieldOf("property").forGetter(BlockPropertyPredicate::propertyName))
+public final class BlockPropertyPredicate extends StateTestingPredicate implements LazyBlockProperty {
+    public static final MapCodec<BlockPropertyPredicate> CODEC = RecordCodecBuilder.mapCodec(instance -> stateTestingCodec(
+            instance).and(Codec.STRING.fieldOf("property").forGetter(BlockPropertyPredicate::propertyName))
             .apply(instance, BlockPropertyPredicate::new));
 
     private final String propertyName;
     private @Nullable IntegerProperty property;
 
-    public BlockPropertyPredicate(String propertyName) {
+    public BlockPropertyPredicate(Vec3i offset, String propertyName) {
+        super(offset);
         this.propertyName = propertyName;
     }
 
+    public BlockPropertyPredicate(String propertyName) {
+        this(Vec3i.ZERO, propertyName);
+    }
+
     public BlockPropertyPredicate(IntegerProperty property) {
-        this(property.getName());
+        this(Vec3i.ZERO, property.getName());
         this.property = property;
     }
 
@@ -36,8 +40,7 @@ public final class BlockPropertyPredicate implements BlockPredicate, LazyBlockPr
     }
 
     @Override
-    public boolean test(LevelAccessor level, BlockPos pos) {
-        BlockState state = level.getBlockState(pos);
+    protected boolean test(BlockState state) {
         IntegerProperty property = this.getProperty(state);
         return property != null && state.getValue(property) < this.getMaxValue(property);
     }
--- Common/src/main/java/fuzs/universalbonemeal/common/world/level/levelgen/blockpredicates/VineFacePredicate.java
+++ Common/src/main/java/fuzs/universalbonemeal/common/world/level/levelgen/blockpredicates/VineFacePredicate.java
@@ -1,25 +1,34 @@
 package fuzs.universalbonemeal.common.world.level.levelgen.blockpredicates;
 
 import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import fuzs.universalbonemeal.common.init.ModRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.core.Vec3i;
 import net.minecraft.world.level.block.VineBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicate;
 import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicateType;
+import net.minecraft.world.level.levelgen.blockpredicates.StateTestingPredicate;
 
 /**
  * @see VineBlock#getUpdatedState(BlockState, net.minecraft.world.level.BlockGetter, BlockPos)
  */
-public record VineFacePredicate() implements BlockPredicate {
-    public static final MapCodec<VineFacePredicate> CODEC = MapCodec.unit(VineFacePredicate::new);
+public final class VineFacePredicate extends StateTestingPredicate {
+    public static final MapCodec<VineFacePredicate> CODEC = RecordCodecBuilder.mapCodec(instance -> stateTestingCodec(
+            instance).apply(instance, VineFacePredicate::new));
+
+    public VineFacePredicate(Vec3i offset) {
+        super(offset);
+    }
+
+    public VineFacePredicate() {
+        this(Vec3i.ZERO);
+    }
 
     @Override
-    public boolean test(LevelAccessor level, BlockPos pos) {
-        BlockState state = level.getBlockState(pos);
+    protected boolean test(BlockState state) {
         for (Direction direction : Direction.Plane.HORIZONTAL) {
             BooleanProperty property = VineBlock.getPropertyForFace(direction);
             if (state.hasProperty(property) && state.getValue(property)) {
